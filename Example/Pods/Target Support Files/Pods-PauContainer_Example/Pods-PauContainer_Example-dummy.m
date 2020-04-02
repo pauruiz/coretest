4bdd7424421eaa3ad42f55f27a8ced5c29aec362
Pau Ruiz <pau@iosnow.net>
2020-04-03 00:16:45 +0100

    Removal of any reference to Cocoapods, created a new project from scratch as a library, and copied the README file (outdated as referenced Cocoapods)  and the LICENSE file

--- Example/Pods/Target Support Files/Pods-PauContainer_Example/Pods-PauContainer_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PauContainer_Example : NSObject
-@end
-@implementation PodsDummy_Pods_PauContainer_Example
-@end
---
+++ PauContainer/PauContainer.h
@@ -0,0 +1,19 @@
+//
+//  PauContainer.h
+//  PauContainer
+//
+//  Created by Pau on 03/04/2020.
+//  Copyright © 2020 iOSNow. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PauContainer.
+FOUNDATION_EXPORT double PauContainerVersionNumber;
+
+//! Project version string for PauContainer.
+FOUNDATION_EXPORT const unsigned char PauContainerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PauContainer/PublicHeader.h>
+
+
